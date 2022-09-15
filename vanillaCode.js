b03176a1409653458263e8f27af4991a722e9251
shubhamzingare <shubhamzingre6@gmail.com>
2022-09-15 18:55:35 +0530

    initial commit

---
+++ vanillaCode.js
@@ -0,0 +1,33 @@
+// input
+let text = document.querySelector("#textInput");
+let btn = document.querySelector("#btnTranslate");
+let outputDiv = document.querySelector("#output");
+
+// URL
+let serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslatedUrl(text){
+        return serverURL + "?" + "text=" + text;
+}
+
+// errorHandler
+function errorHandler(error){
+        alert("Something wrong with server! Try again after some time")
+}
+
+function clicked(){
+        // outputDiv.innerText = "Hi i am Krunal, " + text.value;
+
+        let inputText = text.value;
+        
+        fetch(getTranslatedUrl(inputText)) 
+                .then(response => response.json())
+                .then(json => {
+                        var tranlatedText = json.contents.translated;
+                        outputDiv.innerText = tranlatedText;
+                        })
+                .catch(errorHandler)
+};
+
+// output
+btn.addEventListener("click", clicked);
